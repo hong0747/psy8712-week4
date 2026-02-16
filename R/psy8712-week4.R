2333c8774fb52c4aa0098f0f8a43c39ff947eb22
Julia Hong <hong0747@umn.edu>
2026-02-16 12:58:39 -0600

    Converted datadate to POSIXct, converted values of 0 to missing values within q1 to q5 columns, removed cases that skipped q2, reshaped q1 to q5 columns

--- R/psy8712-week4.R
+++ R/psy8712-week4.R
@@ -7,3 +7,7 @@ import_tbl <- read_delim("../data/week4.dat", delim = "-", show_col_types = TRUE
 glimpse(import_tbl)
 wide_tbl <- import_tbl %>% separate(qs, into = c("q1", "q2", "q3", "q4", "q5"), sep = " - ", convert = TRUE)
 sapply(wide_tbl[5:9], as.integer)
+wide_tbl$datadate <- as.POSIXct(wide_tbl$datadate, format = "%b %d %Y, %H:%M:%S")
+wide_tbl <- wide_tbl %>% mutate(across(q1:q5, na_if, 0))
+wide_tbl <- wide_tbl %>% filter(!is.na(q2))
+long_tbl <- wide_tbl %>% pivot_longer(cols = q1:q5, names_to = "q_num", values_to = "response")
